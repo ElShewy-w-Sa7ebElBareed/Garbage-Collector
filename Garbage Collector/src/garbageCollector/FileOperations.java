13c12a4f7894279f84140d250e81fc72bb5b600b
ahmed <ahmed@192.168.1.6>
2021-05-29 13:10:02 +0200

    spaces

--- Garbage Collector/src/garbageCollector/FileOperations.java
+++ Garbage Collector/src/garbageCollector/FileOperations.java
@@ -5,31 +5,33 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class FileOperations {
 	public static final String UTF8_BOM = "\uFEFF";
-	public static void readheap(String path,ArrayList<Node> heap) {
+
+	public static void readheap(String path, ArrayList<Node> heap) {
 		try {
-			String [] rowData;
-			Scanner reader = new Scanner(new InputStreamReader(new FileInputStream(path),"UTF-8"));
+			String[] rowData;
+			Scanner reader = new Scanner(new InputStreamReader(new FileInputStream(path), "UTF-8"));
 			while (reader.hasNext()) {
 				String row = reader.next();
 				while (row.startsWith(UTF8_BOM)) {
 					row = row.substring(1);
 				}
-				rowData =row.split(",");
-				heap.add(new Node(Integer.parseInt(rowData[0]),Integer.parseInt(rowData[1]),Integer.parseInt(rowData[2])));
+				rowData = row.split(",");
+				heap.add(new Node(Integer.parseInt(rowData[0]), Integer.parseInt(rowData[1]),
+						Integer.parseInt(rowData[2])));
 			}
 			reader.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public static void readRoot(String rootpath,ArrayList<Node> heap,ArrayList<Node> root) {
-		String row ;
+
+	public static void readRoot(String rootpath, ArrayList<Node> heap, ArrayList<Node> root) {
+		String row;
 		int id;
 		try {
-			Scanner reader = new Scanner(new InputStreamReader(new FileInputStream(rootpath),"UTF-8"));
+			Scanner reader = new Scanner(new InputStreamReader(new FileInputStream(rootpath), "UTF-8"));
 			while (reader.hasNext()) {
 				row = reader.next();
 				while (row.startsWith(UTF8_BOM)) {
@@ -37,7 +39,7 @@ public class FileOperations {
 				}
 				id = Integer.parseInt(row);
 				for (Node n : heap) {
-					if (n.ID==id) {
+					if (n.ID == id) {
 						root.add(n);
 						break;
 					}
@@ -48,13 +50,13 @@ public class FileOperations {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void setPointer(String pointerPath,ArrayList<Node> heap) {
-		String row ;
+
+	public static void setPointer(String pointerPath, ArrayList<Node> heap) {
+		String row;
 		String rowData[];
-		int ID1,ID2;
+		int ID1, ID2;
 		try {
-			Scanner reader = new Scanner(new InputStreamReader(new FileInputStream(pointerPath),"UTF-8"));
+			Scanner reader = new Scanner(new InputStreamReader(new FileInputStream(pointerPath), "UTF-8"));
 			while (reader.hasNext()) {
 				row = reader.next();
 				while (row.startsWith(UTF8_BOM)) {
@@ -64,9 +66,9 @@ public class FileOperations {
 				ID1 = Integer.parseInt(rowData[0]);
 				ID2 = Integer.parseInt(rowData[1]);
 				for (Node n1 : heap) {
-					if (n1.ID==ID1) {
+					if (n1.ID == ID1) {
 						for (Node n2 : heap) {
-							if (n2.ID==ID2) {
+							if (n2.ID == ID2) {
 								n1.addChildren(n2);
 								break;
 							}
@@ -80,5 +82,5 @@ public class FileOperations {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- Garbage Collector/src/garbageCollector/Node.java
+++ Garbage Collector/src/garbageCollector/Node.java
@@ -3,25 +3,26 @@ package garbageCollector;
 import java.util.ArrayList;
 
 public class Node {
-    int ID;
-    int memory_start , memory_end ;
-    boolean Marked ;
-    ArrayList<Node> children = new ArrayList<Node>();
-    Node(int ID ,int memory_start,int memory_end ){
-    	this.ID = ID;
-    	this.memory_start = memory_start ;
-    	this.memory_end = memory_end ;
-    	this.Marked = false ;
-    }
-    
-    public void addChildren(Node n) {
-    	children.add(n);
-    }
+	int ID;
+	int memory_start, memory_end;
+	boolean Marked;
+	ArrayList<Node> children = new ArrayList<Node>();
+
+	Node(int ID, int memory_start, int memory_end) {
+		this.ID = ID;
+		this.memory_start = memory_start;
+		this.memory_end = memory_end;
+		this.Marked = false;
+	}
+
+	public void addChildren(Node n) {
+		children.add(n);
+	}
 
 	@Override
 	public String toString() {
 		return "Node [ID=" + ID + ", memory_start=" + memory_start + ", memory_end=" + memory_end + ", Marked=" + Marked
 				+ ", children=" + children + "]";
 	}
-    
- }
+
+}
